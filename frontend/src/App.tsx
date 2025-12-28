a5257cfc25f67b808ece1b92aff9444fd7303035
khanubed <ubedprogrammer@gmail.com>
2025-12-28 12:11:33 +0530

    adding renderPage to navcontext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,14 @@
 import React, { useState , useEffect, useContext} from 'react';
-import { LandingPage } from './components/LandingPage';
-import { AuthPage } from './components/AuthPage';
-import { Dashboard } from './components/Dashboard';
-import { AdminPage } from './components/AdminPage';
-import axios from 'axios'
+
 import {toast, Toaster} from 'react-hot-toast'
-import { AuthContext } from './context/AuthContext';
 import { Routes, Route } from 'react-router-dom';
 import RedirectPage from './components/RedirectPage';
+import { NavContext } from './context/NavigationContext';
 
 
 export default function App() {
 
-  const {  currentPage , userData , setUserData } = useContext(AuthContext);
-
-  // const handleUpgrade = (plan: 'pro' | 'business') => {
-  //   if (!userData) return;
-
-  //     setUserData(prev =>
-  //   prev ? { ...prev, subscriptionPlan: plan } : prev
-  // );
-
-  // };
-
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'landing':
-        return <LandingPage  />;
-      case 'signup':
-        return (
-          <AuthPage
-            mode="signup"
-          />
-        );
-      case 'login':
-        return (
-          <AuthPage
-            mode="login"
-          />
-        );
-      case 'dashboard':
-        return (
-          <Dashboard
-            // onUpgrade={handleUpgrade}
-          />
-        );
-      case 'admin':
-        return (
-          <AdminPage
-          />
-        );
-      default:
-        return <LandingPage  />;
-    }
-  };
+  const {  renderPage } = useContext(NavContext);
 
   return (
     <div className="min-h-screen bg-background">
--- frontend/src/context/NavigationContext.tsx
+++ frontend/src/context/NavigationContext.tsx
@@ -1,4 +1,8 @@
 import { createContext, PropsWithChildren, useState } from "react";
+import { LandingPage } from '../components/LandingPage';
+import { AuthPage } from '../components/AuthPage';
+import { Dashboard } from '../components/Dashboard';
+import { AdminPage } from '../components/AdminPage';
 
 type SubscriptionPlan = 'free' | 'pro' | 'business';
 type Page = 'landing' | 'signup' | 'login' | 'dashboard' | 'admin';
@@ -13,8 +17,41 @@ export const NavProvider = ({ children }: PropsWithChildren) => {
         setCurrentPage(page);
     };
 
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'landing':
+        return <LandingPage  />;
+      case 'signup':
+        return (
+          <AuthPage
+            mode="signup"
+          />
+        );
+      case 'login':
+        return (
+          <AuthPage
+            mode="login"
+          />
+        );
+      case 'dashboard':
+        return (
+          <Dashboard
+            // onUpgrade={handleUpgrade}
+          />
+        );
+      case 'admin':
+        return (
+          <AdminPage
+          />
+        );
+      default:
+        return <LandingPage  />;
+    }
+  };
+
+
   return (
-    <NavContext.Provider value={{ currentPage, setCurrentPage, navigate }}>
+    <NavContext.Provider value={{ currentPage, setCurrentPage, navigate , renderPage}}>
       {children}
     </NavContext.Provider>
   );
